a620531bc76367d2ac0dff91637aaebfaaa1b41b
nithyaswi <teja11manchi@gmail.com>
2020-10-09 18:19:59 +0530

    Add platform

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintlog;
+var chain;
+var bird;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +19,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+   constraintlog = new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -33,9 +37,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body,constraintlog.body);
+   
 }
 
 function draw(){
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
+    constraintlog.display();
 }
